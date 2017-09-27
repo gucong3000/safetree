089b729708d7cfa7c4e25de89f7664cca884dacb
刘祺 <gucong@gmail.com>
2017-09-27 17:21:00 +0800

    update config

--- run.bat
+++ run.bat
@@ -4,6 +4,7 @@ set "CI_TEACHER_ACCOUNT=d3V4aSBoYW5odWFxaWFuZw==" && node_modules\electron\dist\
 set "CI_TEACHER_ACCOUNT=emhvbmdzaGFuIGdvbmdoYW5odWkzNTMx" && node_modules\electron\dist\electron .
 set "CI_TEACHER_ACCOUNT=emhvbmdzaGFuIGh1bWluMTk4MDEwMjc=" && node_modules\electron\dist\electron .
 set "CI_TEACHER_ACCOUNT=emhvbmdzaGFuIGh1eWkxNDkwMTc5" && node_modules\electron\dist\electron .
+set "CI_TEACHER_ACCOUNT=emhvbmdzaGFuIGNhaXFpeGluMzYzMA==" && node_modules\electron\dist\electron .
 set "CI_TEACHER_ACCOUNT=emhvbmdzaGFuIGNoZW5kb25ncWluMTA3Mg==" && node_modules\electron\dist\electron .
 set "CI_TEACHER_ACCOUNT=emhvbmdzaGFuIHpoYW9zaGFubGFu" && node_modules\electron\dist\electron .
 set "CI_TEACHER_ACCOUNT=Y2hlbmdkdSB6aGFvbGluZzQwMDQ=" && node_modules\electron\dist\electron .
