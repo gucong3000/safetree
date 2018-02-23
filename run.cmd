ac519a747b9946d3a75b7398f30f07e71b7e4282
刘祺 <gucong@gmail.com>
2018-02-23 17:23:56 +0800

    在CI中，过滤掉日志中的跨域信息 (#65)

--- run.cmd
+++ run.cmd
@@ -1,4 +1,3 @@
 @echo off
-chcp 65001
-cls
+chcp 65001 >nul 2>nul
 for /F %%a in (account.txt) do set "CI_TEACHER_ACCOUNT=%%a" && npm run -s start
--- run.sh
+++ run.sh
@@ -3,10 +3,16 @@
 while read line
 do
 	export "CI_TEACHER_ACCOUNT=$line"
+	echo -e "$CI_TEACHER_ACCOUNT" | base64 -d
+	echo
 	for VARIABLE in {1..9}
 	do
-		echo -e "$CI_TEACHER_ACCOUNT $VARIABLE"
-		npm run -s start && sleep 5 && break
+		echo -e "第${VARIABLE}次尝试"
+		if [ "${CI-}" ]; then
+			(npm run -s start 2> /dev/null) && sleep 5 && break
+		else
+			npm run -s start && sleep 5 && break
+		fi
 		sleep 1
 	done
 done < account.txt
